cc058a23119a48e8d2f5edb15eb372ea221ac824
pyatrix <37685859+pyatrix@users.noreply.github.com>
2018-03-27 23:48:35 +0800

    Update and rename Rotate_one_picture.py to Rotate_Specified_picture.py

--- Rotate_Specified_picture.py
+++ Rotate_Specified_picture.py
@@ -1,7 +1,7 @@
-# Rotate one picture 360 degree and save it 將一張圖片旋轉360度並保存
-# Preset each rotation angle 預設每次旋轉角度 : 10 degrees
-# Default input 預設輸入: lena.jpg
-# Default output file name 預設輸出檔名與格式 : lena10.png ,lena20.png,lena30.png,.....
+# Rotate one picture 360 degree and save it 
+# Preset each rotation angle  : 10 degrees
+# Default input : lena.jpg
+# Default output file name  : lena10.png ,lena20.png,lena30.png,.....
 
 
 import sys
@@ -11,10 +11,9 @@ import cv2
 angle = 0
 while angle < 30:
 	
-	# Preset each rotation angle 預設每次旋轉角度 : 10 degrees
+	# Preset each rotation angle  : 10 degrees
 	angle += 10
 	
-	# Default input 預設輸入: lena.jpg
 	i = sys.argv[1]
 	print (i)
 	img = cv2.imread('i') 
@@ -26,9 +25,9 @@ while angle < 30:
 	dst = cv2.warpAffine(img,M,(width,height))
 	
 	cv2.imshow('name',dst) 
-	cv2.waitKey(30)
+	cv2.waitKey(23)
 	
-	# Default output file name 預設輸出檔名與格式 : lena10.png ,lena20.png,lena30.png,.....
+	# Default output file name  : lena10.png ,lena20.png,lena30.png,.....
 	if    i[1]=='.' :
 		fo = i [0:1]
 	elif  i[2]=='.' :
@@ -37,5 +36,7 @@ while angle < 30:
 		fo = i [0:3]
 	elif  i[4]=='.' :
 		fo = i [0:4]
+	elif  i[5]=='.' :
+		fo = i [0:5]
 	outname = '%s_%s.png' % ( fo, angle )
 	cv2.imwrite(outname,dst)
